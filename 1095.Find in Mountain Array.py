5290a295b301afa2ec35642954f2c1d2ad18d3d7
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2024-08-15 07:06:32 +0530

    Sync

--- 1095.Find in Mountain Array.py
+++ 1095.Find in Mountain Array.py
@@ -8,23 +8,23 @@
 
 
 class Solution:
-    def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
-        def search(l: int, r: int, k: int) -> int:
-            while l < r:
-                mid = (l + r) >> 1
-                if k * mountain_arr.get(mid) >= k * target:
-                    r = mid
-                else:
-                    l = mid + 1
-            return -1 if mountain_arr.get(l) != target else l
+  def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
+    def search(l: int, r: int, k: int) -> int:
+      while l < r:
+        mid = (l + r) >> 1
+        if k * mountain_arr.get(mid) >= k * target:
+          r = mid
+        else:
+          l = mid + 1
+      return -1 if mountain_arr.get(l) != target else l
 
-        n = mountain_arr.length()
-        l, r = 0, n - 1
-        while l < r:
-            mid = (l + r) >> 1
-            if mountain_arr.get(mid) > mountain_arr.get(mid + 1):
-                r = mid
-            else:
-                l = mid + 1
-        ans = search(0, l, 1)
-        return search(l + 1, n - 1, -1) if ans == -1 else ans
+    n = mountain_arr.length()
+    l, r = 0, n - 1
+    while l < r:
+      mid = (l + r) >> 1
+      if mountain_arr.get(mid) > mountain_arr.get(mid + 1):
+        r = mid
+      else:
+        l = mid + 1
+    ans = search(0, l, 1)
+    return search(l + 1, n - 1, -1) if ans == -1 else ans
