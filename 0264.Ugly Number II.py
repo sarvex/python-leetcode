e580c4191d605fd1556724b4ea972d3357595886
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2024-08-18 10:10:12 +0530

    Sync 264

--- 0264.Ugly Number II.py
+++ 0264.Ugly Number II.py
@@ -1,13 +1,16 @@
+from heapq import heappop, heappush
+
+
 class Solution:
-    def nthUglyNumber(self, n: int) -> int:
-        h = [1]
-        vis = {1}
-        ans = 1
-        for _ in range(n):
-            ans = heappop(h)
-            for v in [2, 3, 5]:
-                nxt = ans * v
-                if nxt not in vis:
-                    vis.add(nxt)
-                    heappush(h, nxt)
-        return ans
+  def nthUglyNumber(self, n: int) -> int:
+    h = [1]
+    vis = {1}
+    ans = 1
+    for _ in range(n):
+      ans = heappop(h)
+      for v in [2, 3, 5]:
+        nxt = ans * v
+        if nxt not in vis:
+          vis.add(nxt)
+          heappush(h, nxt)
+    return ans
