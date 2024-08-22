3c9064c3a065c5358d9a0310bf4a225ec31b9461
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2024-08-22 10:05:08 +0530

    Sync 0476

--- 0476.Number Complement.py
+++ 0476.Number Complement.py
@@ -1,3 +1,3 @@
 class Solution:
-    def findComplement(self, num: int) -> int:
-        return num ^ ((1 << num.bit_length()) - 1)
+  def findComplement(self, num: int) -> int:
+    return num ^ ((1 << num.bit_length()) - 1)
