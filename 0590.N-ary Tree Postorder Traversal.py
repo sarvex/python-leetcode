c09bc7311f668240ea7518b5490ee43c07bbbc74
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2024-08-26 05:41:57 +0530

    Sync 590

--- 0590.N-ary Tree Postorder Traversal.py
+++ 0590.N-ary Tree Postorder Traversal.py
@@ -1,21 +1,22 @@
-"""
+from typing import List
+
+
 # Definition for a Node.
 class Node:
-    def __init__(self, val=None, children=None):
-        self.val = val
-        self.children = children
-"""
+  def __init__(self, val=None, children=None):
+    self.val = val
+    self.children = children
 
 
 class Solution:
-    def postorder(self, root: 'Node') -> List[int]:
-        def dfs(root):
-            if root is None:
-                return
-            for child in root.children:
-                dfs(child)
-            ans.append(root.val)
+  def postorder(self, root: 'Node') -> List[int]:
+    def search(root):
+      if root is None:
+        return
+      for child in root.children:
+        search(child)
+      ans.append(root.val)
 
-        ans = []
-        dfs(root)
-        return ans
+    ans = []
+    search(root)
+    return ans
