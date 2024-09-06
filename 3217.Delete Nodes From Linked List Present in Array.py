2c4146b9dfb328488d2e0df72415a7974206dc2f
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2024-09-06 09:27:49 +0530

    Sync 3217

--- 3217.Delete Nodes From Linked List Present in Array.py
+++ 3217.Delete Nodes From Linked List Present in Array.py
@@ -4,14 +4,12 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def modifiedList(
-        self, nums: List[int], head: Optional[ListNode]
-    ) -> Optional[ListNode]:
-        s = set(nums)
-        pre = dummy = ListNode(next=head)
-        while pre.next:
-            if pre.next.val in s:
-                pre.next = pre.next.next
-            else:
-                pre = pre.next
-        return dummy.next
+  def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+    s = set(nums)
+    pre = dummy = ListNode(next=head)
+    while pre.next:
+      if pre.next.val in s:
+        pre.next = pre.next.next
+      else:
+        pre = pre.next
+    return dummy.next
