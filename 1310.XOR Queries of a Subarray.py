0a386fbe84eee01e64ca5e0498fcc0925b4ec738
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2024-09-16 11:00:35 +0530

    Updated challenge

--- 1310.XOR Queries of a Subarray.py
+++ 1310.XOR Queries of a Subarray.py
@@ -1,4 +1,9 @@
+from itertools import accumulate
+from operator import xor
+from typing import List
+
+
 class Solution:
-    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
-        s = list(accumulate(arr, xor, initial=0))
-        return [s[r + 1] ^ s[l] for l, r in queries]
+  def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+    s = list(accumulate(arr, xor, initial=0))
+    return [s[r + 1] ^ s[l] for l, r in queries]
--- 1684.Count the Number of Consistent Strings.py
+++ 1684.Count the Number of Consistent Strings.py
@@ -1,4 +1,7 @@
+from typing import List
+
+
 class Solution:
-    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
-        s = set(allowed)
-        return sum(all(c in s for c in w) for w in words)
+  def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+    s = set(allowed)
+    return sum(all(c in s for c in w) for w in words)
