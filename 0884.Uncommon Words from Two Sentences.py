ad119e4bde8e18d51932a2c929cff536ef8f9143
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2024-09-18 13:34:38 +0530

    Updated challenge

--- 0884.Uncommon Words from Two Sentences.py
+++ 0884.Uncommon Words from Two Sentences.py
@@ -1,4 +1,8 @@
+from collections import Counter
+from typing import List
+
+
 class Solution:
-    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
-        cnt = Counter(s1.split()) + Counter(s2.split())
-        return [s for s, v in cnt.items() if v == 1]
+  def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+    cnt = Counter(s1.split()) + Counter(s2.split())
+    return [s for s, v in cnt.items() if v == 1]
