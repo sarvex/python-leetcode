6a40603d66c2031c9643861c773ae28c92880622
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2024-09-19 09:53:47 +0530

    Updated challenge

--- 0241.Different Ways to Add Parentheses.py
+++ 0241.Different Ways to Add Parentheses.py
@@ -1,21 +1,25 @@
-class Solution:
-    def diffWaysToCompute(self, expression: str) -> List[int]:
-        @cache
-        def dfs(exp):
-            if exp.isdigit():
-                return [int(exp)]
-            ans = []
-            for i, c in enumerate(exp):
-                if c in '-+*':
-                    left, right = dfs(exp[:i]), dfs(exp[i + 1 :])
-                    for a in left:
-                        for b in right:
-                            if c == '-':
-                                ans.append(a - b)
-                            elif c == '+':
-                                ans.append(a + b)
-                            else:
-                                ans.append(a * b)
-            return ans
+from functools import cache
+from typing import List
 
-        return dfs(expression)
+
+class Solution:
+  def diffWaysToCompute(self, expression: str) -> List[int]:
+    @cache
+    def search(exp):
+      if exp.isdigit():
+        return [int(exp)]
+      ans = []
+      for i, c in enumerate(exp):
+        if c in '-+*':
+          left, right = search(exp[:i]), search(exp[i + 1:])
+          for a in left:
+            for b in right:
+              if c == '-':
+                ans.append(a - b)
+              elif c == '+':
+                ans.append(a + b)
+              else:
+                ans.append(a * b)
+      return ans
+
+    return search(expression)
