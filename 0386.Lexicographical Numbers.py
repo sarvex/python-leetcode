461616db5cc224704a089ab601bba0f2d3898d5e
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2024-09-25 10:15:15 +0530

    Updated challenge

--- 0386.Lexicographical Numbers.py
+++ 0386.Lexicographical Numbers.py
@@ -1,13 +1,16 @@
+from typing import List
+
+
 class Solution:
-    def lexicalOrder(self, n: int) -> List[int]:
-        ans = []
-        v = 1
-        for _ in range(n):
-            ans.append(v)
-            if v * 10 <= n:
-                v *= 10
-            else:
-                while v % 10 == 9 or v + 1 > n:
-                    v //= 10
-                v += 1
-        return ans
+  def lexicalOrder(self, n: int) -> List[int]:
+    ans = []
+    v = 1
+    for _ in range(n):
+      ans.append(v)
+      if v * 10 <= n:
+        v *= 10
+      else:
+        while v % 10 == 9 or v + 1 > n:
+          v //= 10
+        v += 1
+    return ans
