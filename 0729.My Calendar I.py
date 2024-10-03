6fc670f3c12f2c7d1f37e99ae35f541a84a6447a
Sarvex Jatasra <sarvex.jatasra@gmail.com>
2024-10-03 10:47:19 +0530

    Updated challenge

--- 0729.My Calendar I.py
+++ 0729.My Calendar I.py
@@ -2,16 +2,15 @@ from sortedcontainers import SortedDict
 
 
 class MyCalendar:
-    def __init__(self):
-        self.sd = SortedDict()
+  def __init__(self):
+    self.sd = SortedDict()
 
-    def book(self, start: int, end: int) -> bool:
-        idx = self.sd.bisect_right(start)
-        if idx < len(self.sd) and end > self.sd.values()[idx]:
-            return False
-        self.sd[end] = start
-        return True
-
+  def book(self, start: int, end: int) -> bool:
+    idx = self.sd.bisect_right(start)
+    if idx < len(self.sd) and end > self.sd.values()[idx]:
+      return False
+    self.sd[end] = start
+    return True
 
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
